1ccb69693fde28a921472ee1f4c809604060721f
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2022-06-25 00:57:41 +0600

    Create X-PRO.py

---
+++ X-PRO.py
@@ -0,0 +1,33 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from fire64 import Subscraption
+ 
+       Subscraption()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from old32 import bye
+ 
+ 
+        bye()
+ 
+ 
+ 
